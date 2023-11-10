07675ed23f9193a3bc1dce6a729a0cc7ff919212
CallumSoleil <146648713+CallumSoleil@users.noreply.github.com>
2023-11-10 16:45:58 +0000

    First try a 2

---
+++ Coursework/StepCounter.c
@@ -0,0 +1,38 @@
+
+int main() {
+    int menu;
+    printf("
+A: Specify the filename to be imported
+B: Display the total number of records in the file
+C: Find the date and time of the timeslot with the fewest steps
+D: Find the data and time of the timeslot with the largest number of steps
+E: Find the mean step count of all the records in the file
+F: Find the longest continuous period where the step count is above 500 steps
+Q: Exit
+")
+scanf("%d", &menu);
+    switch (menu)
+    {
+        case "A":
+        printf("Illan\n");
+        break;
+        case "B":
+        while (fgets(line_buffer2, buffer_size, file2) != NULL) {
+            
+        break;
+        case "C":
+        printf("Pascal\n");
+        break;
+        case "D":
+        printf("Liam\n");
+        break;
+        case "E":
+        printf("Charlie\n");
+        break;
+        case "F":
+
+        case "Q":
+
+
+return 0
+}
