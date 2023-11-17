7a1fa72e97e836853527beaf917a6811bb39fb0b
CallumSoleil <146648713+CallumSoleil@users.noreply.github.com>
2023-11-17 16:56:57 +0000

    task2

--- Coursework/StepCounter.c
+++ Coursework/StepCounter.c
@@ -1,38 +1,83 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "FitnessDataStruct.h"
 
+// Struct moved to header file
+
+// Define any additional variables here
+// Global variables for filename and FITNESS_DATA array
+
+
+// This is your helper function. Do not change it in any way.
+// Inputs: character array representing a row; the delimiter character
+// Ouputs: date character array; time character array; steps character array
+void tokeniseRecord(const char *input, const char *delimiter,
+                    char *date, char *time, char *steps) {
+    // Create a copy of the input string as strtok modifies the string
+    char *inputCopy = strdup(input);
+    
+    // Tokenize the copied string
+    char *token = strtok(inputCopy, delimiter);
+    if (token != NULL) {        strcpy(date, token);
+    }
+    
+    token = strtok(NULL, delimiter);
+    if (token != NULL) {
+        strcpy(time, token);
+    }
+    
+    token = strtok(NULL, delimiter);
+    if (token != NULL) {
+        strcpy(steps, token);
+    }
+    
+    // Free the duplicated string
+    free(inputCopy);
+
+                    }
+
+
+
+
+// Complete the main function
 int main() {
-    int menu;
-    printf("
-A: Specify the filename to be imported
-B: Display the total number of records in the file
-C: Find the date and time of the timeslot with the fewest steps
-D: Find the data and time of the timeslot with the largest number of steps
-E: Find the mean step count of all the records in the file
-F: Find the longest continuous period where the step count is above 500 steps
-Q: Exit
-")
-scanf("%d", &menu);
-    switch (menu)
-    {
-        case "A":
-        printf("Illan\n");
-        break;
-        case "B":
-        while (fgets(line_buffer2, buffer_size, file2) != NULL) {
-            
-        break;
-        case "C":
-        printf("Pascal\n");
-        break;
-        case "D":
-        printf("Liam\n");
-        break;
-        case "E":
-        printf("Charlie\n");
-        break;
-        case "F":
+   
+    char menu;
+    char filename[100];
+    while (menu != 'Q' | menu != 'q'){
+    printf("\nA: Specify the filename to be imported\nB: Display the total number of records in the file\nC: Find the date and time of the timeslot with the fewest steps\nD: Find the data and time of the timeslot with the largest number of steps\nE: Find the mean step count of all the records in the file\nF: Find the longest continuous period where the step count is above 500 steps\nQ: Exit \n");
+    scanf("%c", &menu);
 
-        case "Q":
+    switch (menu){
+        case 'A':
+        printf("Input file name: ");
+        scanf("%s", filename);
 
+        FILE *file = fopen(filename, "r");
 
-return 0
+        if (!file){
+            printf("Error: could not open file\n");
+        }
+        break;
+
+        case 'B':
+        break;
+
+        case 'C':
+        break;
+
+        case 'D':
+        break;
+
+        case 'E':
+        break;
+
+        case 'F':
+        break;
+        
+        default:
+        printf("Invalid choice\n");
+    }
+    }
 }
